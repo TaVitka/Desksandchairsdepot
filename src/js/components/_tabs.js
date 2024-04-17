dc7e0a07a35e2f1d810df2458309b0c49462f7e3
TaVitka <tavitka17@gmail.com>
2024-04-18 02:27:34 +0300

    added js for catalog tabs

---
+++ src/js/components/_tabs.js
@@ -0,0 +1,46 @@
+const tabsContainer = document.querySelector('.tabs');
+const tabsList = tabsContainer?.querySelector('.tabs__nav');
+const tabButtons = tabsList?.querySelectorAll('.tabs__btn');
+const tabPanels = tabsContainer?.querySelectorAll('.tabs__pane > div');
+
+tabButtons?.forEach((tab, index) => {
+  tab.setAttribute('role', 'tab');
+  if (index === 0) {
+    tab.classList.add('selected');
+  } else {
+    tabPanels[index].setAttribute('hidden', '');
+  }
+});
+
+tabsList?.addEventListener('click', e => {
+  const clickedTab = e.target.closest('.tabs__btn');
+  const allTabs = tabsList.querySelectorAll('.tabs__btn');
+
+  tabsContainer.scrollIntoView({
+    behavior: 'smooth',
+  });
+
+  if (!clickedTab) return;
+  e.preventDefault();
+
+  switchTab(clickedTab);
+
+  allTabs.forEach(tab => {
+    tab.classList.remove('selected');
+  });
+
+  clickedTab.classList.add('selected');
+});
+
+function switchTab(newTab) {
+  const activePanelId = newTab.getAttribute('data-btn');
+  const activePanel = tabsContainer.querySelectorAll(`[data-id="${activePanelId}"]`);
+
+  tabPanels?.forEach(panel => {
+    panel.setAttribute('hidden', true);
+  });
+
+  activePanel.forEach(active => {
+    active.removeAttribute('hidden', false);
+  })
+}
