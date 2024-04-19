cd88b9a1335252ca3a0d74339e702f35b9e2faef
TaVitka <tavitka17@gmail.com>
2024-04-19 07:39:13 +0300

    added some changes to js

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,15 +2,3 @@ import './components/_burger';
 import './components/_slider';
 import './components/_tabs';
 import './components/_popup';
-
-document.querySelectorAll('.request-btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    document.querySelector('#form-request').classList.add('open');
-  });
-});
-
-document.addEventListener('click', e => {
-  if (e.target.id === 'form-request') {
-    document.querySelector('#form-request').classList.remove('open');
-  }
-});
